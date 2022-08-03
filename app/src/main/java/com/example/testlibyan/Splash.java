19e91030bb0171bba4c649fade532bf178ce31d5
raadfortest <raad.elmsellati@pcc.edu>
2022-08-03 01:51:01 -0700

    Made final changes to splash screen

--- app/src/main/java/com/example/testlibyan/Splash.java
+++ app/src/main/java/com/example/testlibyan/Splash.java
@@ -12,7 +12,20 @@ public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(Splash.this, MainActivity.class));
-        finish();
+        setContentView(R.layout.activity_splash);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(Splash.this, MainActivity.class));
+                }
+            }
+        };
+        thread.start();
     }
 }
