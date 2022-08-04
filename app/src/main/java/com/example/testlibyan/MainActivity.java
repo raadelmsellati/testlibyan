9a6d97c51f26097904791f012ddceb6f82d676da
raadfortest <raad.elmsellati@pcc.edu>
2022-08-03 20:24:49 -0700

    Made final changes to splash screen

--- app/src/main/java/com/example/testlibyan/MainActivity.java
+++ app/src/main/java/com/example/testlibyan/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,14 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // loading Animation from
+        final Animation animation = AnimationUtils.loadAnimation(this, R.anim.bounce);
+
+        // getting the Button from activity_main.xml file
+        final ImageView welcome = findViewById(R.id.welcome);
+        welcome.startAnimation(animation);
     }
 
-    public void showAtbeg() {
 
-    }
 
-    public void BtnClicked(View view){
+    /*public void BtnClicked(View view){
         TextView txtWelcome = findViewById(R.id.txtwelcome);
         txtWelcome.setText("Hello Raad");
-    }
+    }*/
 }
