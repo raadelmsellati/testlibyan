d6b45a5fc12db0da9a3b4c3f66ef5cfaab0dd327
raadfortest <raad.elmsellati@pcc.edu>
2022-08-04 14:57:16 -0700

    Half Login Auth

--- app/src/main/java/com/example/testlibyan/MainActivity.java
+++ app/src/main/java/com/example/testlibyan/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.testlibyan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText register_user, register_password, register_email;
     private Button createB, cancelB;
     private ImageView pp_register;
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
         dialogBuilder = new AlertDialog.Builder(this);
         final View registerPopup = getLayoutInflater().inflate(R.layout.popup, null);
 
+        mAuth = FirebaseAuth.getInstance();
 
         register_user = (EditText) registerPopup.findViewById(R.id.inputUser);
         register_password = (EditText) registerPopup.findViewById(R.id.inputPassword);
@@ -65,8 +76,54 @@ public class MainActivity extends AppCompatActivity {
 
         createB.setOnClickListener(new View.OnClickListener() {
             @Override
+            //When the user click create
             public void onClick(View view) {
                 //Still need to be connected to DB
+                String email = register_email.getText().toString().trim();
+                String username = register_user.getText().toString().trim();
+                String password = register_password.getText().toString().trim();
+
+                if(email.isEmpty())
+                {
+                    register_email.setError("Email is required!");
+                    register_email.requestFocus();
+                    return;
+                }
+                if(username.isEmpty())
+                {
+                    register_user.setError("Username is required");
+                    register_user.requestFocus();
+                    return;
+                }
+                if(password.isEmpty())
+                {
+                    register_password.setError("Password is required");
+                    register_user.requestFocus();
+                    return;
+                }
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+                {
+                    register_email.setError("Email is not valid");
+                    register_email.requestFocus();
+                    return;
+                }
+                if(password.length() < 6)
+                {
+                    register_password.setError("Password should be at least 6 characters");
+                    register_password.requestFocus();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful())
+                            }
+                        }) {
+
+                }
             }
         });
 
@@ -77,6 +134,50 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    /*public void onClick(View v){
+        if(v.getId()==R.id.create)
+        {
+            registerInfo();
+        }
+    }*/
+
+    /* public void registerInfo(View view) {
+        String email = register_email.getText().toString().trim();
+        String username = register_user.getText().toString().trim();
+        String password = register_password.getText().toString().trim();
+
+        if(email.isEmpty())
+        {
+            register_email.setError("Email is required!");
+            register_email.requestFocus();
+            return;
+        }
+        if(username.isEmpty())
+        {
+            register_user.setError("Username is required");
+            register_user.requestFocus();
+            return;
+        }
+        if(password.isEmpty())
+        {
+            register_password.setError("Password is required");
+            register_user.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            register_email.setError("Email is required!");
+            register_email.requestFocus();
+            return;
+        }
+        if(password.length() < 6)
+        {
+            register_password.setError("Password should be at least 6 characters");
+            register_password.requestFocus();
+            return;
+        }
+    } */
+
     //-----------------------------------------
 
 
---
+++ app/src/main/java/com/example/testlibyan/User.java
@@ -0,0 +1,4 @@
+package com.example.testlibyan;
+
+public class User {
+}
